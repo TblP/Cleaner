11218259ffbc4f0d03c13d6edcfd9257720751f8
TblP AI <vczypl@e.et>
2022-03-31 19:37:48 +0300

    zipcheck

--- WinFormsApp/Form1.cs
+++ WinFormsApp/Form1.cs
@@ -1,4 +1,6 @@
 
+using System.IO.Compression;
+
 namespace WinFormsApp
 {
     public partial class Antimalware : Form
@@ -25,8 +27,7 @@ namespace WinFormsApp
                 else
                 {
                     textBox1.Text = Path.GetDirectoryName(opd.FileName);
-                }
-                
+                } 
             }
         }
 
@@ -41,10 +42,65 @@ namespace WinFormsApp
             ResultForm result = new ResultForm();
             result.Show();
         }
-
+        string Path2;
+        string temppath = @"temp";
+        ZipArchive archive;
         private void button1_Click(object sender, EventArgs e)
         {
+            if (textBox1.Text.Contains(".zip"))
+            {
+                Path2 = textBox1.Text;
+                string destFile = Path.Combine(temppath,"test.zip");
+                File.Copy(Path2, destFile);
+                ZipChecker(destFile);
+            }
+        }
+        
+        public void ZipChecker(string path)
+        {
+            try
+            {
+                archive = ZipFile.OpenRead(path);
 
+            }catch (Exception ex)
+            {
+                return;
+            }
+
+            foreach (var entry in archive.Entries)
+            {
+                MessageBox.Show(entry.FullName);
+                if (entry.FullName.Contains(".zip"))
+                {
+                    string fullPath = Path.Combine(temppath, entry.FullName);
+                    if (String.IsNullOrEmpty(entry.Name))
+                    {
+                        Directory.CreateDirectory(fullPath);
+                    }
+                    else
+                    {
+                        entry.ExtractToFile(fullPath);
+                    }
+                }
+                else
+                {
+                    var stream = entry.Open();
+                    var reader = new StreamReader(stream);  
+                    MessageBox.Show(reader.ReadToEnd());
+                    //функцию проверки файла
+                }
+            }
+            archive.Dispose();
+            File.Delete(path);
+            if (Directory.EnumerateFiles(temppath, "*.*", SearchOption.AllDirectories).Any())
+            {
+                string[] fileNames = Directory.GetFiles(temppath, "*.*", SearchOption.AllDirectories);
+                foreach (var fileName in fileNames)
+                {
+                    MessageBox.Show(fileName);
+                    ZipChecker(Path.GetFullPath(fileName)); 
+                }
+            }
         }
     }
 }
